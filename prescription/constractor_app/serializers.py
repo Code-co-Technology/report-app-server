5cf8322cbaa713f009a0f79e5960fd3828873c11
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2024-10-25 17:54:24 +0500

    'success'

--- prescription/constractor_app/serializers.py
+++ prescription/constractor_app/serializers.py
@@ -21,36 +21,56 @@ class ConstractorPrescriptionSerializer(serializers.ModelSerializer):
         return {
             'create_at': obj.project.opening_date,
         }
+    
+    def get_owner(self, obj):
+        full_name = obj.owner.first_name + ' ' + obj.owner.last_name
+        return {
+            'name': full_name
+        }
 
 
 class ConstractorPrescriptionsSerializer(serializers.ModelSerializer):
     prescription = ConstractorPrescriptionSerializer(read_only=True)
+    contractor = UserInformationContractorSerializer(read_only=True)
     status = serializers.CharField(source='get_status_display')
     user = serializers.SerializerMethodField()
-    contractor = UserInformationContractorSerializer(read_only=True)
 
     class Meta:
         model = PrescriptionContractor
-        fields = ['id', 'prescription', 'contractor', 'user', 'status', 'create_at']
+        fields = ['id', 'prescription', 'contractor', 'user', 'status']
 
-    def get_user(self, obj):
-        if obj.user:
-            return {
-                'first_name': obj.user.first_name,
-                'last_name': obj.user.last_name,
-            }
-        return None 
+    def get_user(self,obj):
+        return {
+            "full_name": obj.user.first_name + ' ' + obj.user.last_name
+        }
+
 
 
 class ConstractorPrescriptionsUpddateSerializer(serializers.ModelSerializer):
+    prescription_comment = serializers.ListField(child = serializers.CharField(max_length = 1000000),
+        write_only=True, required=False)
+    prescription = serializers.IntegerField(write_only=True, required=False)
 
     class Meta:
         model = PrescriptionContractor
-        fields = ['id', 'prescription', 'user', 'status']
+        fields = ['id', 'prescription', 'prescription_comment', 'user']
 
     def update(self, instance, validated_data):
         instance.user = validated_data.get('user', instance.user)
-        # Update instance fields
+
+        prescription_comment = validated_data.pop('prescription_comment', [])
+        prescription = validated_data.pop('prescription', None)
+        owner = self.context.get('owner')
+        
+        if prescription:
+            try:
+                prescription = Prescriptions.objects.get(id=prescription)
+            except Prescriptions.DoesNotExist:
+                raise serializers.ValidationError("The provided prescription does not exist.")
+        else:
+            prescription = None
+        for comment_text in prescription_comment:
+            PrescriptionsComment.objects.create(prescription=prescription, owner=owner, comment=comment_text)
         instance.save()
         return instance
 
